4e1d92c33afc8cc172bfdff106621058e42f42d4
JayceDevelops <jayceiezzi@gmail.com>
2026-02-10 12:15:59 -0500

    Finished up home page and started about me page

---
+++ function.js
@@ -0,0 +1,19 @@
+// Handles Home Tab Click
+function homeClick() {
+    window.location.replace("/index.html");
+}
+
+// Handles About Me Tab Click
+function aboutClick() {
+    window.location.replace("/about-me/aboutme.html");
+}
+
+// Handles Experience Tab Click
+function experienceClick() {
+    window.location.replace("/experience/experience.html");
+}
+
+// Handles Projects Tab Click
+function projectClick() {
+    window.location.replace("/project/projects.html");
+}
